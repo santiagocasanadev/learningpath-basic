db69807fe450610333990106a3ddf610b3eda420
Santiago Casana <72729667+chant96@users.noreply.github.com>
2024-08-07 17:46:55 -0500

    Initial commit

---
+++ src/main/java/com/nttdata/learningpath_basic/LearningpathBasicApplication.java
@@ -0,0 +1,13 @@
+package com.nttdata.learningpath_basic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningpathBasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningpathBasicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nttdata/learningpath_basic/LearningpathBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nttdata.learningpath_basic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningpathBasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
